c182ac2033291c755e31416bf0fe710731be7a3f
Eduardo Raad <eduardo.raad@datilmedia.com>
2016-09-14 12:44:58 -0500

    Adds web interface.

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+import transform
+from flask import Flask, request, make_response
+from jinja2 import Environment, PackageLoader, Template
+
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+# Setup web app and templates
+env = Environment(loader=PackageLoader('app', 'templates'))
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        props = {"init_sequence": int(request.form["init_sequence"]),
+                 "branch_code": request.form["branch_code"],
+                 "pos_code": request.form["pos_code"],
+                 "fixed_address": "",
+                 "fixed_phone": "",
+                 "product_code": request.form["product_code"],
+                 "product_aux_code": "",
+                 "id_type": request.form["id_type"]}
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = "datil_" + file.filename
+            content = file.stream.read()
+            response = make_response(transform.datilize(content, props))
+            response.headers["Content-Disposition"] = "attachment; filename=" + filename
+            return response
+    template = env.get_template('index.html')
+    return template.render()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- transform.py
+++ transform.py
@@ -1,57 +1,47 @@
 import csv
+import StringIO
 
-BRANCH_CODE = ""
-POS_CODE = ""
-INIT_SEQUENCE = 0
-ISSUE_DATE = ""
-ID_TYPE = ""
-PRODUCT_CODE = ""
-PRODUCT_AUX_CODE = ""
-FIXED_ADDRESS = ""
-FIXED_PHONE = ""
-INPUT_FILE = ""
-OUTPUT_FILE = ""
+def csv2string(data):
+    si = StringIO.StringIO()
+    writer = csv.writer(si, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    for row in data:
+        writer.writerow(row)
+    return si.getvalue()
 
-ifile = open(INPUT_FILE, "rb")
-reader = csv.reader(ifile)
-rownum = 0
-newlist = []
-invoice_seq = INIT_SEQUENCE
+def datilize(ifile, properties):
+    rownum = 0
+    newlist = []
+    invoice_seq = properties["init_sequence"]
+    reader = csv.reader(StringIO.StringIO(ifile))
+    print reader
 
-for row in reader:
-    if rownum == 0:
-        headers = row[3:7]
-        rownum += 1
-    else:
-        newrow = [BRANCH_CODE,
-                  POS_CODE,
-                  invoice_seq,
-                  row[0],
-                  row[1],
-                  ID_TYPE,
-                  row[2],
-                  FIXED_ADDRESS,
-                  FIXED_PHONE,
-                  PRODUCT_CODE,
-                  PRODUCT_AUX_CODE]
-        items = row[3:7]
-        for index, item in enumerate(items):
-            if item:
-                newlist.append(newrow + \
-                               [headers[index]] + \
-                               [item] + \
-                               [0.00] + \
-                               [1] + \
-                               ["14%"] + \
-                               [""])
-        rownum += 1
-        invoice_seq += 1
+    for row in reader:
+        if rownum == 0:
+            headers = row[3:7]
+            rownum += 1
+        else:
+            newrow = [properties["branch_code"],
+                      properties["pos_code"],
+                      invoice_seq,
+                      row[0],
+                      row[1],
+                      properties["id_type"],
+                      row[2],
+                      properties["fixed_address"],
+                      properties["fixed_phone"],
+                      properties["product_code"],
+                      properties["product_aux_code"]]
+            items = row[3:7]
+            for index, item in enumerate(items):
+                if item:
+                    newlist.append(newrow + \
+                                   [headers[index]] + \
+                                   [item] + \
+                                   [0.00] + \
+                                   [1] + \
+                                   ["14%"] + \
+                                   [""])
+            rownum += 1
+            invoice_seq += 1
 
-ofile = open(OUTPUT_FILE, "wb")
-writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-for row in newlist:
-    writer.writerow(row)
-
-ifile.close()
-ofile.close()
+    return csv2string(newlist)
